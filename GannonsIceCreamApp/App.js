a93166c818c6dbd3449214b5603e4984f6bd1004
wlong123 <wlong2017@pingry.org>
2018-08-14 12:09:14 -0400

    switched from expo to native code

--- GannonsIceCreamApp/App.js
+++ GannonsIceCreamApp/App.js
@@ -1,28 +1,49 @@
-import React from 'react';
-import { createStackNavigator } from 'react-navigation';
-import Home from './src/Components/home'
-import Login from './src/Components/Customer/login';
-import CreateAccount from './src/Components/Customer/createAccount';
-import CustomerHome from './src/Components/Customer/customer-home';
-import EmployeeHome from './src/Components/Employee/employee-home';
-import Employee from './src/Components/Employee/employee';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
 
-export default class App extends React.Component {
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
   render() {
-    return <RootStack/>;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    );
   }
 }
 
-const RootStack = createStackNavigator(
-  {
-    Home: Home,
-    CustomerHome: CustomerHome,
-    Login: Login,
-    CreateAccount: CreateAccount,
-    EmployeeHome: EmployeeHome,
-    Employee: Employee
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
-  {
-    initialRouteName: 'Home',
-  }
-);
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
--- README.md
+++
@@ -1,3 +0,0 @@
-# GannonsIceCreamApp
-
-This app was created using react-native and keeps track of purchases made by customers at Gannon's Ice Cream shop. 
